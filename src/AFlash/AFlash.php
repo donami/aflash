135e1ac38efd3cfd98f418d31a16c523141c9cc4
donami <d0namiz@gmail.com>
2015-06-15 15:43:41 +0200

    Changes

--- src/AFlash/AFlash.php
+++ src/AFlash/AFlash.php
@@ -3,23 +3,94 @@
 namespace donami\AFlash;
 
 /**
- * A utility class to easy creating and handling of forms
+ * Class for handling messages and notifications
  *
- * @package CForm
+ * @package AFlash
  */
 class AFlash
 {
-
+    private $htmlBefore = '<div class="alert-box %s">';
+    private $htmlAfter  = '</div>';
 
     /**
      * Constructor
      *
-     * @param array $form     details for the form
-     * @param array $elements all the elements
+     * @return void
      */
-    public function __construct($form = [], $elements = [])
+    public function __construct()
     {
-    	die("HEJ");
+        if (!array_key_exists('messages', $_SESSION)) {
+            $_SESSION['messages'] = array();
+        }
+    }
+
+    /**
+     * Add a message
+     * 
+     * @param string $type
+     * @param strng $message
+     * @param string $location
+     *  
+     * @return void
+     */
+    public function add($type, $message, $location = null)
+    {
+        $_SESSION['messages'][$type][] = $message;
+
+        if (!is_null($location)) {
+            header("location: " . $location);
+            die();
+        }
+    }
+
+    /**
+     * Clear the added messages
+     * 
+     * @return void
+     */
+    private function clear()
+    {
+        $_SESSION['messages'] = array();
+    }
+
+    /**
+     * Display the messages
+     * 
+     * @return string
+     */
+    public function display()
+    {
+        $messages = $_SESSION['messages'];
+
+        $html = '';
+
+        foreach ($messages as $type => $value) {
+            switch (strtoupper($type)) {
+                case 'E':
+                    $class = 'error';
+                    break;
+                
+                case 'S':
+                    $class = 'success';
+                    break;
+
+                default:
+                    $class = 'notification';
+                    break;
+            }
+
+            $html .= sprintf($this->htmlBefore, $class);
+
+            foreach ($value as $k => $msg) {
+                $html .= sprintf('<div>%s</div>', $msg);
+            }
+
+            $html .= $this->htmlAfter;
+        }
+
+        $this->clear();
+
+        return $html;
     }
 
 
